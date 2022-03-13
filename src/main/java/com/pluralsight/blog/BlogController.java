eb5b0f13edb10a9b3d2b39d41c843df85e3ec16e
Zoltan Nagy <zoltan.nagy@veeva.com>
2022-03-13 19:38:29 +0100

    Display Static Data from the Post Repository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,10 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
+
+        List<Post> posts = postRepository.getAllPosts();
+
+        System.out.println(posts.get(0).getTitle());
         
-        modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts", posts);
         return "home";
     }
 
